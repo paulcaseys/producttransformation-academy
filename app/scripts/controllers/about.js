d2833d0c4fed8ea3a57b7a2374cf0b5a3d5f6627
Paul Casey <paulcaseys@gmail.com>
2015-10-22 17:31:36 +1100

    adding some standards to controllers

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,9 +9,45 @@
  */
 angular.module('angularBoilerplateApp')
   .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    var el = this;
+
+    // initialises on view
+    el.init = function(){
+        // logs the view
+        el.testMethod();
+
+    };
+
+
+    // method for the console
+    el.testMethod = function(){
+       console.log('AboutCtrl testMethod');
+    };
+
+
+    /*******************
+     * TRANSITION IN/OUT
+     * these are called when the view is routed in / out
+     */
+
+    // transitions the view in
+    el.transitionIn = function(){
+        // basic way to hide element
+        //$(this.el).hide();
+    },
+
+    // removes all eventlisteners
+    el.transitionOut = function(){
+        // basic way to hide element
+        //$(this.el).hide();
+    },
+
+
+    // inits it all
+    el.init();
+
+
+
   });
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,44 @@
  */
 angular.module('angularBoilerplateApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+
+    var el = this;
+
+    // initialises on view
+    el.init = function(){
+        // logs the view
+        el.testMethod();
+
+    };
+
+
+    // method for the console
+    el.testMethod = function(){
+       console.log('MainCtrl testMethod');
+    };
+
+
+    /*******************
+     * TRANSITION IN/OUT
+     * these are called when the view is routed in / out
+     */
+
+    // transitions the view in
+    el.transitionIn = function(){
+        // basic way to hide element
+        //$(this.el).hide();
+    },
+
+    // removes all eventlisteners
+    el.transitionOut = function(){
+        // basic way to hide element
+        //$(this.el).hide();
+    },
+
+
+    // inits it all
+    el.init();
+
+
   });
