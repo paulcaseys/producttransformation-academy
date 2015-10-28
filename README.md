7a259aea41095ad97f63c9dcb4ca57d82bbe5989
paulcaseys <paulcaseys@gmail.com>
2015-10-28 04:14:28 -0400

    adding gulp support

--- README.md
+++ README.md
@@ -5,7 +5,13 @@ version 0.12.1.
 
 ## Build & development
 
+this app is set up with both grunt and gulp. simply rename the package-grunt-ver.json or the package-gulp-ver.json to package.json, depending on your preference
+
+with grunt
 Run `grunt` for building and `grunt serve` for preview.
+
+with gulp
+run `gulp dist` for building and `gulp` for preview
 
 ## Testing
 
---
+++ gulpfile.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var gulp = require('gulp'),
+  modRewrite = require('connect-modrewrite'),
+  webpack = require('webpack-stream');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'del', 'browser-sync']
+});
+
+// Constants
+var BUILD_DIR = 'build/';
+var TMP_DIR = '.tmp/';
+
+// Webpack
+gulp.task('webpack:vendor', function() {
+  return gulp.src('app/scripts/vendor.js')
+    .pipe(webpack({
+      output: {
+        filename: "vendor.js"
+      }
+    }))
+    .pipe(gulp.dest(TMP_DIR + 'scripts/'))
+});
+
+gulp.task('webpack', function() {
+  return gulp.src('app/scripts/entry.js')
+    .pipe(webpack({
+      module: {
+        loaders: [
+          { test: /\.json$/, loader: "json-loader" }
+        ]
+      },
+      output: {
+        filename: "app.js"
+      }
+    }))
+    .pipe(gulp.dest(TMP_DIR + 'scripts/'))
+    .pipe($.browserSync.reload({stream:true}));
+});
+
+
+
+// Html
+gulp.task('html:dev', function() {
+  return gulp.src(TMP_DIR + 'views/index.html')
+    .pipe(gulp.dest(TMP_DIR))
+    .pipe($.browserSync.reload({stream:true}));
+});
+
+gulp.task('html:dist', function() {
+  return gulp.src(BUILD_DIR + 'views/index.html')
+    .pipe(gulp.dest(BUILD_DIR))
+});
+
+// Sass
+/*gulp.task('sass', function () {
+  return $.rubySass('app/styles/main.scss', { sourcemap: true })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(TMP_DIR + 'styles'))
+    .pipe($.browserSync.reload({stream:true}));
+});*/
+
+// Images
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe(gulp.dest(BUILD_DIR + 'images/'))
+    .pipe($.browserSync.reload({stream:true}));
+});
+
+// .htaccess
+gulp.task('htaccess', function () {
+  return gulp.src('.htaccess')
+    .pipe(gulp.dest(BUILD_DIR));
+});
+
+// Static server
+gulp.task('serve:dev', ['dev'], function() {
+  $.browserSync({
+    server: {
+      baseDir: [".", TMP_DIR, "app"],
+      middleware: [
+        modRewrite([
+          '!\\.html|\\.js|\\.css|\\.png|\\.jpg|\\.gif|\\.svg|\\.txt$ /index.html [L]'
+        ])
+      ]
+    }
+  });
+});
+
+gulp.task('serve:dist', function() {
+  $.browserSync({
+    server: {
+      baseDir: [BUILD_DIR],
+      middleware: [
+        modRewrite([
+          '!\\.html|\\.js|\\.css|\\.png|\\.jpg|\\.gif|\\.svg|\\.txt$ /index.html [L]'
+        ])
+      ]
+    }
+  });
+});
+
+// e2e tests
+gulp.task('webdriver-update', $.protractor.webdriver_update);
+gulp.task('protractor', ['webdriver-update'], function () {
+  gulp.src(['test/e2e/**/*.js'])
+    .pipe($.protractor.protractor({
+      configFile: "test/protractor.config.js",
+      args: ['--baseUrl', 'http://localhost:3000']
+    }))
+    .on('error', function (e) {
+      throw e
+    });
+});
+
+// Clean
+gulp.task('clean', function () {
+  $.del.sync([TMP_DIR + '*', BUILD_DIR + '*']);
+});
+
+// Development
+gulp.task('dev', ['clean', 'webpack', 'webpack:vendor', 'sass', 'html:dev']);
+
+// Default Task (Dev environment)
+gulp.task('default', ['serve:dev'], function() {
+  // Scripts
+  gulp.watch(['config.json', 'app/scripts/**/*.js'], ['webpack']);
+
+
+  // Htmls
+  gulp.watch(TMP_DIR + 'views/**/*.html', ['html:dev']);
+
+  // Styles
+  gulp.watch('app/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('deps', ['html:dist'], function () {
+  var assets = $.useref.assets();
+
+  return gulp.src([BUILD_DIR + 'index.html'])
+    // Concatenates asset files from the build blocks inside the HTML
+    .pipe(assets)
+    // Appends hash to extracted files app.css → app-098f6bcd.css
+    .pipe($.rev())
+    // Adds AngularJS dependency injection annotations
+    .pipe($.if('*.js', $.ngAnnotate()))
+    // Uglifies js files
+    .pipe($.if('*.js', $.uglify()))
+    // Minifies css files
+    .pipe($.if('*.css', $.csso()))
+    // Brings back the previously filtered HTML files
+    .pipe(assets.restore())
+    // Parses build blocks in html to replace references to non-optimized scripts or stylesheets
+    .pipe($.useref())
+    // Rewrites occurences of filenames which have been renamed by rev
+    .pipe($.revReplace())
+    // Minifies html
+    .pipe($.if('*.html', $.minifyHtml({
+      empty: true,
+      spare: true,
+      quotes: true
+    })))
+    // Creates the actual files
+    .pipe(gulp.dest(BUILD_DIR))
+    // Print the file sizes
+    .pipe($.size({ title: BUILD_DIR, showFiles: true }));
+});
+
+// Distribution
+gulp.task('prepare', ['dev', 'images', 'htaccess']);
+gulp.task('dist', ['prepare', 'deps']);
