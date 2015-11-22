451db0ae3d8893c4f3698f00439680c3b45c121d
paulcaseys <paulcaseys@gmail.com>
2015-11-22 01:56:16 -0500

    catering for back/forward button page changes

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -13,9 +13,9 @@ angular.module('angularBoilerplateApp')
     // Must use a wrapper object, otherwise "activeItem" won't work
     $scope.states = {};
 
-
+    // navigation items
     $scope.items = [{
-        id: 'home',
+        id: '',
         title: 'Home',
         path: '#/'
     }, {
@@ -29,24 +29,32 @@ angular.module('angularBoilerplateApp')
     }];
 
     // declare default activeItem
-    $scope.states.activeItem = 'home';
-
-    $scope.changePage = function(id){
-        $location.path(id);
-        $scope.states.activeItem = id;
-    };
-    
-    var path = "#"+$location.path();
-    var lookup = {};
+    $scope.states.activeItem = '';
 
     // checks which item matches the path
-    for (var i = 0, len = $scope.items.length; i < len; i++) {
-        lookup = $scope.items[i];
-        if($scope.items[i].path === path){
-            $scope.states.activeItem = $scope.items[i].id;
+    $scope.changeActiveItem = function(){
+        var path = "#"+$location.path();
+        var lookup = {};
+        for (var i = 0, len = $scope.items.length; i < len; i++) {
+            lookup = $scope.items[i];
+            if($scope.items[i].path === path){
+                $scope.states.activeItem = $scope.items[i].id;
+            }
         }
-    }
-    
+    };
+
+    // changes page from click
+    $scope.changePage = function(id){
+        $location.path(id);
+        $scope.changeActiveItem();
+    };
+
+    // caters for back button etc
+    $scope.$on("$locationChangeStart", function() { 
+        $scope.changeActiveItem();
+    });
+
+    $scope.changeActiveItem();
     console.log('NavigationController activeItem : '+$scope.states.activeItem);
 
 });
