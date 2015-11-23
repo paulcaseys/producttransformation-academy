b6ae6a0856225fcca8afff25424646e6a5591062
paulcaseys <paulcaseys@gmail.com>
2015-11-23 02:27:48 -0500

    adding error handling

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -11,15 +11,17 @@ angular.module('angularBoilerplateApp')
   .controller('GalleryCtrl', function ($scope, $http) {
 
 	$scope.items = null;
+	$scope.loadingStatus = "loading";
 
 	$http.get('http://cosmosis-api.com/api/service/data/format/jsonp/?project_name=SummerAtTarget&project_password=6CB4816A23A965B5DFD58E45F4C23&table=unique_references&batch=1&batchSize=6&whereConditionArray=project_id||9&select=*&orderBy=vote_count||desc&callback=?')
-         .success(function (data) {
-             $scope.items = JSON.parse(data.substring(1, data.length-1).substr(1));
-             console.log($scope.items);
-         })
+        .success(function (data) {
+            $scope.items = JSON.parse(data.substring(1, data.length-1).substr(1));
+			$scope.loadingStatus = "loaded";
+            console.log($scope.items);
+        })
 		.error(function () {
-             //  Do some error handling here
-         });
+			$scope.loadingStatus = "error";
+        });
 
   });
 
