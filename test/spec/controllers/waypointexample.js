d7226cdafff8067bd8e7f953c5aadf88fbd2f37e
paulcaseys <paulcaseys@gmail.com>
2015-11-23 02:14:45 -0500

    updating waypoint test

--- test/spec/controllers/waypointexample.js
+++ test/spec/controllers/waypointexample.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Controller: WaypointexamplectrlCtrl', function () {
+describe('Controller: WaypointCtrl', function () {
 
   // load the controller's module
   beforeEach(module('angularBoilerplateApp'));
 
-  var WaypointexamplectrlCtrl,
+  var WaypointCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    WaypointexamplectrlCtrl = $controller('WaypointexamplectrlCtrl', {
+    WaypointCtrl = $controller('WaypointCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(WaypointexamplectrlCtrl.awesomeThings.length).toBe(3);
+    expect(WaypointCtrl.awesomeThings.length).toBe(3);
   });
 });
